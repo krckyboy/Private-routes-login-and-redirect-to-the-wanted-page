9201ecb93d1b01f24aec5bc58122868ad4a5b106
krckyboy <dusan.todorovic.2016@gmail.com>
2019-02-02 14:25:27 +0100

    Added more comments for Private Route

--- src/index.js
+++ src/index.js
@@ -57,7 +57,12 @@ class Login extends Component {
   If user is authenticated already, then we just render the 
   Component through a route, passing in the props (...rest) 
   down to the Route, and then again from the rendered component
-  to the Component. */
+  to the Component. 
+  
+  1. It has the same API as <Route />.
+  2. It renders a <Route /> and passes all the props through to it.
+  3. It checks if the user is authenticated, if they are, it renders
+  the “component” prop. If not, it redirects the user to /login.*/
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
